ea5157bf5edea69fda02ef03781a2a320e3f6e86
firstdoubletripledev <firstdoubletripledev@gmail.com>
2020-07-21 13:25:30 +0200

    New

---
+++ sites/eCommerce.dd/files/php/twig/5f09978fb8f80___string_template__d8bb77_3XcMiekAXfXTMeOzTbA_rK5UB/pyQSm66YVzx6BLPrVDKcvzl0-eUjq43bbhwOHC-XbrM.php
@@ -0,0 +1,86 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__d8bb77bb69e4d49544b2b1fdd2b56161348e52730a290b27e2c08dba71813770 */
+class __TwigTemplate_b69a556d22854b06cf934b03d8e443fa3e384c4112737ba51db7d3465202cc3c extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 1];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "catalog/";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["tid"] ?? null)), "html", null, true);
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__d8bb77bb69e4d49544b2b1fdd2b56161348e52730a290b27e2c08dba71813770";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__d8bb77bb69e4d49544b2b1fdd2b56161348e52730a290b27e2c08dba71813770", "");
+    }
+}
